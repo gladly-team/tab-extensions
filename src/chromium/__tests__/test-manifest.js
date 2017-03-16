74dbbc80f51819bf32e4a3d789adf7c7f0c0aa9e
Kevin Jennison <kevin.jennison1@gmail.com>
2017-03-16 13:21:34 -0700

    More manifest tests.

--- src/chromium/__tests__/test-manifest.js
+++ src/chromium/__tests__/test-manifest.js
@@ -10,7 +10,26 @@ test('manifest permissions have not changed', () => {
 });
 
 test('content script permissions have not changed', () => {
-  var permissions = manifest['content_scripts'];
-  expect(permissions).toBeUndefined();
+  expect(manifest['content_scripts']).toBeUndefined();
 });
 
+test('manifest does not extend devtools', () => {
+  expect(manifest['devtools_page']).toBeUndefined();
+});
+
+test('manifest does not use plugins', () => {
+  expect(manifest['plugins']).toBeUndefined();
+});
+
+// Test extension updating URL.
+
+test('extension update URL is set to Chrome Web Store', () => {
+  var updateEndpoint = 'http://clients2.google.com/service/update2/crx';
+  expect(manifest['update_url']).toBe(updateEndpoint);
+});
+
+// Basic display tests.
+
+test('extension name is correct', () => {
+  expect(manifest['name']).toBe('Tab for a Cause');
+});
