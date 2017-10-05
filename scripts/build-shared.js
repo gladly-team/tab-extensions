768609a739c2ca4d6da8f1c467b7adcd2776ddb1
Kevin Jennison <kevin.jennison1@gmail.com>
2017-10-05 11:08:33 -0700

    Lint fixes.

--- scripts/build-shared.js
+++ scripts/build-shared.js
@@ -1,25 +1,25 @@
 
-var fs = require('fs-extra');
-var path = require('path');
+var fs = require('fs-extra')
+var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var webpack = require('webpack');
+var webpack = require('webpack')
 
-var SRC_DIR = path.join(__dirname, '../src/shared/');
-var BUILD_DIR = path.join(__dirname, '../intermediate-builds/shared/');
-var HTML_PATH = path.join(SRC_DIR, 'iframe.html');
+var SRC_DIR = path.join(__dirname, '../src/shared/')
+var BUILD_DIR = path.join(__dirname, '../intermediate-builds/shared/')
+var HTML_PATH = path.join(SRC_DIR, 'iframe.html')
 
 if (process.env.NODE_ENV !== 'production') {
-  throw new Error('Builds must have NODE_ENV=production');
+  throw new Error('Builds must have NODE_ENV=production')
 }
 
 // Empty the build directory.
-fs.emptyDirSync(BUILD_DIR);
+fs.emptyDirSync(BUILD_DIR)
 
 var config = {
   entry: path.join(SRC_DIR, 'js/index.js'),
   output: {
-      path: BUILD_DIR,
-      filename: 'js/newtab.js'
+    path: BUILD_DIR,
+    filename: 'js/newtab.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -54,17 +54,17 @@ var config = {
       }
     })
   ]
-};
+}
 
 webpack(config).run((err, stats) => {
   if (err) {
     console.log('Failed to compile.', err)
     process.exit(1)
   }
-  console.log('Webpack JS build complete.');
-});
+  console.log('Webpack JS build complete.')
+})
 
 // Copy other files.
-fs.copySync(path.join(SRC_DIR, 'img'), path.join(BUILD_DIR, 'img'));
+fs.copySync(path.join(SRC_DIR, 'img'), path.join(BUILD_DIR, 'img'))
 
-console.log('Copied shared files to intermediate build.');
+console.log('Copied shared files to intermediate build.')
