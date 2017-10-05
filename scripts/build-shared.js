0c121e9d3e53d7b96d32aaee02c4f2209c9f9e2f
Kevin Jennison <kevin.jennison1@gmail.com>
2017-10-05 10:29:19 -0700

    Uglify JS.

--- scripts/build-shared.js
+++ scripts/build-shared.js
@@ -18,7 +18,22 @@ var config = {
   output: {
       path: path.join(BUILD_DIR, 'js'),
       filename: 'newtab.js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    })
+  ]
 };
 
 webpack(config).run((err, stats) => {
