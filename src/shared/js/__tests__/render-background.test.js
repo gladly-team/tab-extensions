149ff106edf3bad9d994d8db06c8f3fc78631591
Kevin Jennison <kevin.jennison1@gmail.com>
2017-10-11 13:46:25 -0700

    Add more tests.

--- src/shared/js/__tests__/render-background.test.js
+++ src/shared/js/__tests__/render-background.test.js
@@ -71,5 +71,93 @@ describe('background rendering in extension new tab page', () => {
       .toHaveBeenCalledWith('#FFFF00')
   })
 
-  // TODO: test malformed data and no data.
+  it('does not render when the background option is not set', () => {
+    const backgroundMock = require('../background')
+    backgroundMock.getBackgroundSettings = jest.fn(() => {
+      return {
+        backgroundOption: null,
+        customImage: null,
+        backgroundColor: null,
+        imageURL: null
+      }
+    })
+    const renderBackground = require('../render-background').default
+    renderBackground()
+    expect(backgroundMock.showBackgroundColor)
+      .not.toHaveBeenCalled()
+    expect(backgroundMock.showBackgroundImg)
+      .not.toHaveBeenCalled()
+  })
+
+  it('does not render when the custom image is missing', () => {
+    const backgroundMock = require('../background')
+    backgroundMock.getBackgroundSettings = jest.fn(() => {
+      return {
+        backgroundOption: 'custom',
+        customImage: null,
+        backgroundColor: null,
+        imageURL: null
+      }
+    })
+    const renderBackground = require('../render-background').default
+    renderBackground()
+    expect(backgroundMock.showBackgroundColor)
+      .not.toHaveBeenCalled()
+    expect(backgroundMock.showBackgroundImg)
+      .not.toHaveBeenCalled()
+  })
+
+  it('does not render when the color is missing', () => {
+    const backgroundMock = require('../background')
+    backgroundMock.getBackgroundSettings = jest.fn(() => {
+      return {
+        backgroundOption: 'color',
+        customImage: null,
+        backgroundColor: null,
+        imageURL: null
+      }
+    })
+    const renderBackground = require('../render-background').default
+    renderBackground()
+    expect(backgroundMock.showBackgroundColor)
+      .not.toHaveBeenCalled()
+    expect(backgroundMock.showBackgroundImg)
+      .not.toHaveBeenCalled()
+  })
+
+  it('does not render when the daily image is missing', () => {
+    const backgroundMock = require('../background')
+    backgroundMock.getBackgroundSettings = jest.fn(() => {
+      return {
+        backgroundOption: 'daily',
+        customImage: null,
+        backgroundColor: null,
+        imageURL: null
+      }
+    })
+    const renderBackground = require('../render-background').default
+    renderBackground()
+    expect(backgroundMock.showBackgroundColor)
+      .not.toHaveBeenCalled()
+    expect(backgroundMock.showBackgroundImg)
+      .not.toHaveBeenCalled()
+  })
+
+  it('does not render when the image is missing', () => {
+    const backgroundMock = require('../background')
+    backgroundMock.getBackgroundSettings = jest.fn(() => {
+      return {
+        backgroundOption: 'photo',
+        customImage: null,
+        backgroundColor: null,
+        imageURL: null
+      }
+    })
+    const renderBackground = require('../render-background').default
+    renderBackground()
+    expect(backgroundMock.showBackgroundColor)
+      .not.toHaveBeenCalled()
+    expect(backgroundMock.showBackgroundImg)
+      .not.toHaveBeenCalled()
+  })
 })
