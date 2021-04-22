760a45d324a0091829f2a64ef52b2c487758e34c
Kevin Jennison <kevin.jennison1@gmail.com>
2021-04-22 09:12:08 -0700

    Experiment with client-side new tab redirect in Chrome (no URL highlighting)

--- src/chromium/ext-background.js
+++ src/chromium/ext-background.js
@@ -21,22 +21,3 @@ try {
 } catch (e) {
   console.error(e)
 }
-
-// Listen for new tabs.
-const NEW_TAB_URL = 'http://tab.gladly.io/newtab/'
-chrome.tabs.onCreated.addListener(async (tab) => {
-  console.log('created tab', tab)
-  // TODO
-  // Check if this is a blank new tab (not opened by clicking a link).
-  // var isBlankTab = tab.url === 'chrome://newtab/'
-  // const isBlankTab = true
-
-  // if (isBlankTab) {
-  //   // Redirect to Tab for a Cause new tab page.
-  //   await chrome.tabs.update(tab.id, { url: NEW_TAB_URL })
-  // }
-
-  // Redirect to Tab for a Cause new tab page.
-  const updatedTab = await chrome.tabs.update(tab.id, { url: NEW_TAB_URL })
-  console.log('updated tab', updatedTab)
-})
