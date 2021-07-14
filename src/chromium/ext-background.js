666b7974a95fdbb14fbb8784f45fba47a50ba32e
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-07-13 21:32:51 -0400

    updated tabs.create call to be more concise

--- src/chromium/ext-background.js
+++ src/chromium/ext-background.js
@@ -16,8 +16,7 @@ chrome.runtime.onInstalled.addListener(function (object) {
 // https://developer.chrome.com/docs/extensions/reference/action/#event-onClicked
 chrome.action.onClicked.addListener(function (tab) {
   try {
-    const newTabURL = 'chrome://newtab'
-    chrome.tabs.create({ url: newTabURL })
+    chrome.tabs.create()
   } catch (e) {
     console.error(e)
   }
