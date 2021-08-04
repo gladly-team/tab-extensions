146e6c09f03dfeb5bcb7d0adbf0eedd4c5979a15
Kevin Jennison <kevin.jennison1@gmail.com>
2021-08-04 10:43:08 -0400

    Replace template with Tab resources

--- src/safari/Tab for a Cause/Tab for a Cause Extension/Resources/background.js
+++ src/safari/Tab for a Cause/Tab for a Cause Extension/Resources/background.js
@@ -1,6 +1,14 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-});
+browser.runtime.onInstalled.addListener(function (object) {
+  try {
+    // On install, open a welcome tab.
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+      const postInstallURL = 'https://tab.gladly.io/newtab/first-tab/'
+      browser.tabs.create({ url: postInstallURL })
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+// `browser.runtime.setUninstallURL` is not currently supported in Safari.
